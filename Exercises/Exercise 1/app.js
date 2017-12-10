a7469006f905437cb7a4c75858919a1f0c433eec
Lucas Dimas <lucasdcavalcante@gmail.com>
2017-12-10 11:51:21 -0200

    Resolvido o exercicio 1

--- Exercises/Exercise 1/app.js
+++ Exercises/Exercise 1/app.js
@@ -1,10 +1,14 @@
 new Vue({
-    el: '#app',
+    el: '#exercise',
     data: {
         name: 'Lucas Dimas',
-        age: 27
+        age: 27,
+        image: 'http://i0.kym-cdn.com/entries/icons/mobile/000/005/608/nyan-cat-01-625x450.jpg'
     },
     methods: {
+        randomNumber : function (){
+            return Math.random();
+        }
 
     }
 });
